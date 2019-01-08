3eb653942296c9ad7f23b03055b0d1d83a4b1a16
ykptke <ykpteke@gmail.com>
2019-01-08 09:25:33 +0300

    Updates

---
+++ precache-manifest.d86ed1caf55946d171e1e88d2e4e7b56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b738ff157d23b985944f",
+    "url": "/static/css/main.d2981bd8.chunk.css"
+  },
+  {
+    "revision": "b738ff157d23b985944f",
+    "url": "/static/js/main.b738ff15.chunk.js"
+  },
+  {
+    "revision": "6cc7ae7bbd81cf8a0f36",
+    "url": "/static/js/1.6cc7ae7b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2abc7087bf548a44f95d594ddeb7390a",
+    "url": "/index.html"
+  }
+];
